1e698c948b16967d9a96b3caad986a5f20169e5c
Kady Yip <kadyy1998@gmail.com>
2023-11-19 00:19:16 -0500

    Refactor code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-library
+
+This is an library project that comes from The Odin Project's Full Stack Javascript course. It allows users to add books to the library and displays its info. This project demonstrates intermediate Javascript skills, in particular with object constructors.
--- script.js
+++ script.js
@@ -12,6 +12,18 @@ function addBookToLibrary(book) {
 }
 
 function displayBooks() {
+    clearBooks();
+    for (let i = 0; i < myLibrary.length; i++) {
+        const book = document.createElement('div');
+        book.classList.add('book');
+        book.setAttribute('data-index', i);
+        addRemoveButton(book);
+        addBookInfo(book);
+        addReadStatusButton(book);
+    }
+}
+
+function clearBooks() {
     const read = document.querySelector('.read-books');
     const notRead = document.querySelector('.not-read-books');
     while (read.firstChild) {
@@ -20,78 +32,70 @@ function displayBooks() {
     while (notRead.firstChild) {
         notRead.removeChild(notRead.lastChild);
     }
-    for (let i = 0; i < myLibrary.length; i++) {
-        // create book
-        const myBook = myLibrary[i];
-        const book = document.createElement('div');
-        
-        book.classList.add('book');
-        book.setAttribute('data-index', i);
-        const removeButton = document.createElement('button');
-        removeButton.classList.add("close")
-        removeButton.textContent = "×";
-        removeButton.addEventListener("click", (event) => {
-            console.log(book.dataset.index);
-            myLibrary.pop(book.dataset.index);
-            displayBooks();
-        });
-        book.appendChild(removeButton);
-        const bookInfo = document.createElement('div');
-        bookInfo.classList.add('book-info');
-        book.appendChild(bookInfo);
-        for (let prop in myBook) {
-            if (prop != "read") {
-                const propDiv = document.createElement('div');
-                propDiv.classList.add(prop);
-                propDiv.textContent = `${capitalize(prop)}: ${myBook[prop]}`;
-                bookInfo.appendChild(propDiv);
-            }
+}
+
+function addRemoveButton(book) {
+    const removeButton = document.createElement('button');
+    removeButton.classList.add("close")
+    removeButton.textContent = "×";
+    removeButton.addEventListener("click", () => {
+        myLibrary.pop(book.dataset.index);
+        displayBooks();
+    });
+    book.appendChild(removeButton);
+}
+
+function addBookInfo(book) {
+    const myBook = myLibrary[book.dataset.index];
+    const bookInfo = document.createElement('div');
+    bookInfo.classList.add('book-info');
+    book.appendChild(bookInfo);
+    for (let prop in myBook) {
+        if (prop != "read") {
+            const propDiv = document.createElement('div');
+            propDiv.classList.add(prop);
+            propDiv.textContent = `${capitalize(prop)}: ${myBook[prop]}`;
+            bookInfo.appendChild(propDiv);
         }
-        const readButton = document.createElement('button');
-        readButton.classList.add("move");
-        readButton.addEventListener("click", (event) => {
-            myLibrary[book.dataset.index].read = !myLibrary[book.dataset.index].read;
-            displayBooks();
-        });
-        if (myBook.read) {
-            read.appendChild(book);
-            readButton.textContent = "Move to To Read";
-        }
-        else {
-            notRead.appendChild(book);
-            readButton.textContent = "Move to Completed";
-        }
-        book.appendChild(readButton);
     }
 }
 
-function createBookHmtl(book) {
-    
+function addReadStatusButton(book) {
+    const read = document.querySelector('.read-books');
+    const notRead = document.querySelector('.not-read-books');
+    const myBook = myLibrary[book.dataset.index];
+    const readStatusButton = document.createElement('button');
+    readStatusButton.classList.add("move");
+    readStatusButton.addEventListener("click", (event) => {
+        myLibrary[book.dataset.index].read = !myLibrary[book.dataset.index].read;
+        displayBooks();
+    });
+    if (myBook.read) {
+        read.appendChild(book);
+        readStatusButton.textContent = "Move to To Read";
+    }
+    else {
+        notRead.appendChild(book);
+        readStatusButton.textContent = "Move to Completed";
+    }
+    book.appendChild(readStatusButton);
 }
-
 
 function capitalize(str) {
     return str[0].toUpperCase() + str.slice(1);
 }
+
 const dialog = document.querySelector("dialog");
 const newBookButton = document.querySelector(".new-book");
 const addBookButton = document.querySelector(".add-book");
 const closeButton = document.querySelector("dialog .close");
 
-
-let book1 = new Book("Pride and Prejudice", "Jane Austen", 300, true);
-let book2 = new Book("Emma", "Jane Austen", 200, false); 
-
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-displayBooks();
-
-// "Show the dialog" button opens the dialog modally
+// open dialog when button is pressed
 newBookButton.addEventListener("click", () => {
     dialog.showModal();
 });
 
-// "Add book" button adds book to library closes the dialog
+// add book to library when button is pressed
 addBookButton.addEventListener("click", (event) => {
     event.preventDefault();
     const form = document.querySelector("form");
@@ -99,12 +103,18 @@ addBookButton.addEventListener("click", (event) => {
     const author = document.querySelector("#author").value;
     const pages = document.querySelector("#pages").value;
     const read = document.querySelector("#read").checked;
+
+    // check if all required fields are filled in and display error if not
     if (!(title && author && pages)) {
-        const errorMsg = document.createElement('div');
-        errorMsg.classList.add('error-msg');
-        errorMsg.textContent = "Please fill in required fields!"
-        form.prepend(errorMsg);
+        const errorMsg = document.querySelector(".error-msg");
+        if (errorMsg === null) {
+            const errorMsg = document.createElement('div');
+            errorMsg.classList.add('error-msg');
+            errorMsg.textContent = "Please fill in required fields!"
+            form.prepend(errorMsg);
+        }
     }
+
     else {
         const errorMsg = document.querySelector(".error-msg");
         if (errorMsg !== null) {
@@ -118,8 +128,7 @@ addBookButton.addEventListener("click", (event) => {
     }
 });
 
-// "Close" button closes the dialog
+// close the dialog when button is pressed
 closeButton.addEventListener("click", () => {
     dialog.close();
 });
-
